17e22f2fb12dabf40d907bdf7e4ae9997d7621b9
Michael Bumann <michael@railslove.com>
2009-02-22 18:08:58 +0100

    added upload database yml taks

--- lib/railslove/recipes/db.rb
+++ lib/railslove/recipes/db.rb
@@ -25,6 +25,13 @@ namespace :db do
     rake("db:create")
   end
   
+  desc "Upload the database yml from the /config/database.yml"
+  task :upload_config_yml, :roles => :db do
+    put open("config/database.yml").read, "#{shared_path}/database.yml"
+    run "ln -nfs #{shared_path}/database.yml #{release_path}/config/database.yml"
+  end
+  
+  
   desc "create a backup of the mysql database save it to shared/backups/database and download it to /backups"
   task :backup, :roles => :db, :only => { :primary => true } do 
     filename = "#{application}.dump.#{Time.now.to_i}.sql.bz2"
