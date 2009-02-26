47709d0476c7b9f34ac17581b2c5881c300855ee
Michael Bumann <michael@railslove.com>
2009-02-26 14:50:47 +0100

    added sphinx cap recipes

---
+++ lib/railslove/recipes/sphinx.rb
@@ -0,0 +1,36 @@
+namespace :sphinx do
+  
+  desc "Configure Thinkig Sphinx"
+  task :config, :roles => :app do 
+    run "cd #{current_path}; && rake ts:config RAILS_ENV=#{fetch(:rails_env, "production")}"
+  end
+  
+  desc "Start Thinkig Sphinx"
+  task :start, :roles => :app do 
+    run "cd #{current_path}; && rake ts:start RAILS_ENV=#{fetch(:rails_env, "production")}"    
+  end
+  
+  desc "Stop Thinkig Sphinx"
+  task :stop, :roles => :app do 
+    run "cd #{current_path}; && rake ts:stop RAILS_ENV=#{fetch(:rails_env, "production")}"
+  end
+  
+  desc "Run Thinkig Sphinx indexing"
+  task :index, :roles => :app do 
+    run "cd #{current_path}; && rake ts:index RAILS_ENV=#{fetch(:rails_env, "production")}"
+  end
+  
+  desc "Bootstrap Thinkig Sphinx"
+  task :bootstrap, :roles => :app do 
+    run "cd #{current_path}; && rake ts:stop RAILS_ENV=#{fetch(:rails_env, "production")}"
+    run "cd #{current_path}; && rake ts:config RAILS_ENV=#{fetch(:rails_env, "production")}"
+    run "cd #{current_path}; && rake ts:index RAILS_ENV=#{fetch(:rails_env, "production")}"
+    run "cd #{current_path}; && rake ts:start RAILS_ENV=#{fetch(:rails_env, "production")}"
+  end
+  
+  desc "Symlink Sphinx index DB"
+  task :symlink, :roles => :app do
+    run "rm -rf #{release_path}/db/sphinx && ln -nfs #{shared_path}/sphinx #{release_path}/db/sphinx"
+  end
+  
+end
