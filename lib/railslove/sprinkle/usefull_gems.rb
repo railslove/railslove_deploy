64e39383b33cb6eb8b9c9d695f576571d4ff6b75
Michael Bumann <michael@railslove.com>
2009-03-02 22:26:00 +0100

    added daemons to useful gems

--- lib/railslove/sprinkle/usefull_gems.rb
+++ lib/railslove/sprinkle/usefull_gems.rb
@@ -1,5 +1,5 @@
 package :usefull_gems do
-  USEFUL_GEMS ="aws-s3 bcrypt-ruby chronic dm-core dm-more erubis eventmachine facebooker graticule haml hpricot json json_pure memcache-client merb money mime-types oauth ParseTree RedCloth rfacebook ruby-openid ruby2ruby RubyInline sinatra starling thor thoughtbot-factory_girl thoughtbot-shoulda will_paginate xml-simple xmpp4r xmpp4r-simple"
+  USEFUL_GEMS ="aws-s3 bcrypt-ruby chronic daemons dm-core dm-more erubis eventmachine facebooker graticule haml hpricot json json_pure memcache-client merb money mime-types oauth ParseTree RedCloth rfacebook ruby-openid ruby2ruby RubyInline sinatra starling thor thoughtbot-factory_girl thoughtbot-shoulda will_paginate xml-simple xmpp4r xmpp4r-simple"
   gem USEFUL_GEMS
   
   verify do
