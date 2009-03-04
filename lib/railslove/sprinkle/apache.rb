0146e9577721c04b6fe10e9ed2d917a908cf45a0
Michael Bumann <michael@railslove.com>
2009-03-04 20:39:03 +0100

    enable deflate and expire modules by default

--- lib/railslove/sprinkle/apache.rb
+++ lib/railslove/sprinkle/apache.rb
@@ -2,6 +2,8 @@ package :apache, :provides => :webserver do
   description 'Apache2 web server.'
   apt 'apache2 apache2-dev apache2.2-common apache2-mpm-prefork apache2-utils libexpat1 ssl-cert' do
     post :install, 'a2enmod rewrite'
+    post :install, 'a2enmod deflate'
+    post :install, 'a2enmod expires'
   end
   
   verify do
