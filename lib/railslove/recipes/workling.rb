56307c09289555f7baa051ed1729ae8faf20de26
Michael Bumann <michael@railslove.com>
2009-03-09 02:48:02 +0100

    use release_path to start working client

--- lib/railslove/recipes/workling.rb
+++ lib/railslove/recipes/workling.rb
@@ -2,18 +2,18 @@ namespace :workling do
   
   desc "Restart Workling Client"
   task :restart, :roles => :app do 
-    run "cd #{current_path}; script/workling_client stop RAILS_ENV=#{fetch(:rails_env, "production")}"
-    run "cd #{current_path}; script/workling_client start RAILS_ENV=#{fetch(:rails_env, "production")}"
+    run "cd #{release_path}; script/workling_client stop RAILS_ENV=#{fetch(:rails_env, "production")}"
+    run "cd #{release_path}; script/workling_client start RAILS_ENV=#{fetch(:rails_env, "production")}"
   end
   
   desc "Start Workling Client"
   task :start, :roles => :app do 
-    run "cd #{current_path}; script/workling_client start RAILS_ENV=#{fetch(:rails_env, "production")}"
+    run "cd #{release_path}; script/workling_client start RAILS_ENV=#{fetch(:rails_env, "production")}"
   end
   
   desc "Stop Workling Client"
   task :stop, :roles => :app do 
-    run "cd #{current_path}; script/workling_client stop RAILS_ENV=#{fetch(:rails_env, "production")}"
+    run "cd #{release_path}; script/workling_client stop RAILS_ENV=#{fetch(:rails_env, "production")}"
   end
   
 end
