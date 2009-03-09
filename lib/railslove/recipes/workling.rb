4c5a501cd033fd5b4bf651624ffe7dc5d3f21749
Michael Bumann <michael@railslove.com>
2009-03-09 02:06:51 +0100

    added workling recipes

---
+++ lib/railslove/recipes/workling.rb
@@ -0,0 +1,26 @@
+namespace :workling do
+  
+  desc "Restart Workling Client"
+  task :restart, :roles => :app do 
+    run "cd #{current_path}; script/workling_client stop RAILS_ENV=#{fetch(:rails_env, "production")}"
+    run "cd #{current_path}; script/workling_client start RAILS_ENV=#{fetch(:rails_env, "production")}"
+  end
+  
+  desc "Start Workling Client"
+  task :start, :roles => :app do 
+    run "cd #{current_path}; script/workling_client start RAILS_ENV=#{fetch(:rails_env, "production")}"
+  end
+  
+  desc "Stop Workling Client"
+  task :stop, :roles => :app do 
+    run "cd #{current_path}; script/workling_client stop RAILS_ENV=#{fetch(:rails_env, "production")}"
+  end
+  
+end
+
+namespace :starling do
+  desc "Start Starling"
+  task :start do
+    run "starling -d -p #{fetch(:rails_env, "15151")}"
+  end
+end
