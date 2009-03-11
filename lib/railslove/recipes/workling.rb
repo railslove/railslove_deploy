4a851a078d408aa07d700f4c9c461f64f6624729
Michael Bumann <michael@railslove.com>
2009-03-11 05:18:08 +0100

    use current_path again in workling recipes

--- lib/railslove/recipes/workling.rb
+++ lib/railslove/recipes/workling.rb
@@ -2,18 +2,18 @@ namespace :workling do
   
   desc "Restart Workling Client"
   task :restart, :roles => :app do 
-    run "cd #{release_path}; RAILS_ENV=#{fetch(:rails_env, "production")} script/workling_client stop"
-    run "cd #{release_path}; RAILS_ENV=#{fetch(:rails_env, "production")} script/workling_client start"
+    run "cd #{current_path}; RAILS_ENV=#{fetch(:rails_env, "production")} script/workling_client stop"
+    run "cd #{current_path}; RAILS_ENV=#{fetch(:rails_env, "production")} script/workling_client start"
   end
   
   desc "Start Workling Client"
   task :start, :roles => :app do 
-    run "cd #{release_path}; RAILS_ENV=#{fetch(:rails_env, "production")} script/workling_client start"
+    run "cd #{current_path}; RAILS_ENV=#{fetch(:rails_env, "production")} script/workling_client start"
   end
   
   desc "Stop Workling Client"
   task :stop, :roles => :app do 
-    run "cd #{release_path}; RAILS_ENV=#{fetch(:rails_env, "production")} script/workling_client stop"
+    run "cd #{current_path}; RAILS_ENV=#{fetch(:rails_env, "production")} script/workling_client stop"
   end
   
 end
