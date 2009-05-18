95b11a17c0bdc0a7d6ef0597a9c76877977cb583
Michael Bumann <michael@railslove.com>
2009-05-19 01:29:09 +0200

    install imagemagick via apt-get

--- lib/railslove/sprinkle/image_magick.rb
+++ lib/railslove/sprinkle/image_magick.rb
@@ -1,15 +1,5 @@
 package :image_magick do
-  
-  source "http://image_magick.veidrodis.com/image_magick/ImageMagick-6.3.2-9.zip"
-  
-  requires :freeimage
-end
-
-package :freeimage do
-  source "http://downloads.sourceforge.net/freeimage/FreeImage3110.zip"
-  
-end
-
-package :image_magick_essentials do
+  description "Installs imagemagick"
+  apt "imagemagick"
   
 end
