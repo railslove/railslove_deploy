db05bfaabf502228035ea34bb4fbd5ccbfe3cc56
Michael Bumann <michael@railslove.com>
2009-05-29 20:57:19 +0200

    use init script to restart memcached

--- lib/railslove/recipes/memcache.rb
+++ lib/railslove/recipes/memcache.rb
@@ -1,17 +1,19 @@
 namespace :memcache do 
   
   desc "Start Memcached"
-  task :start do
-    sudo "memcached -d -m #{memcache_memory} -l #{memcache_ip} -p #{memcache_port}"
+  task :start, :roles => :cache do
+    sudo "/etc/init.d/memcached start"
+    #sudo "memcached -d -m #{memcache_memory} -l #{memcache_ip} -p #{memcache_port}"
   end
   
   desc "Stop Memcached using killall"
-  task :stop do
-    sudo "killall memcached; echo 0"
+  task :stop, :roles => :cache do
+    sudo "/etc/init.d/memcached stop"
+    #sudo "killall memcached; echo 0"
   end
   
   desc "Restart Memcached"
-  task :restart do 
+  task :restart, :roles => :cache do 
     stop
     start
   end
