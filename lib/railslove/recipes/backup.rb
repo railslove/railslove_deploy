75dd397a7dc8f4f5f9b57443d376ba1244c7a546
Michael Bumann <michael@railslove.com>
2009-06-09 14:47:20 +0200

    use fullpath to astrails-safe

--- lib/railslove/recipes/backup.rb
+++ lib/railslove/recipes/backup.rb
@@ -2,7 +2,7 @@ namespace :backup do
   
   desc "Create a new backup using astrails-safe"
   task :run do 
-    sudo "cd #{current_path}; astrails-safe -v #{current_path}/config/server/backup.rb"
+    sudo "cd #{current_path}; /usr/local/ruby-enterprise/bin/astrails-safe -v #{current_path}/config/server/backup.rb"
   end
   
 end
