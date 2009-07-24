ee408f7a2dd66af5528c6811a34e0eb4799516f4
Michael Bumann <michael@railslove.com>
2009-07-24 10:57:52 +0200

    fix path for monitrc

--- lib/railslove/recipes/monit.rb
+++ lib/railslove/recipes/monit.rb
@@ -3,7 +3,7 @@ namespace :monit do
   desc "Setup the monitrc and enable monit on the server"
   task :setup do 
     put render("config/server/monitrc.erb", binding), "/tmp/monitrc"
-    sudo "mv /tmp/monitrc etc/monit/monit.rc"
+    sudo "mv /tmp/monitrc /etc/monit/monit.rc"
     sudo "echo 'startup=1' | sudo tee -a /etc/default/monit"
   end
   
