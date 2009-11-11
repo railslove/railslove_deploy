b70212f91f155db0cf3f1c55dc8fe240c8d63106
Michael Bumann <michael@derbumi.com>
2009-11-11 14:08:14 +0100

    update monit package to work with the official sprinkle gem

--- lib/railslove/sprinkle/monit.rb
+++ lib/railslove/sprinkle/monit.rb
@@ -1,27 +1,33 @@
 package :monit, :provides => :monitoring do
   description 'installs monit - a system monitoring utility which allows an admin to easily monitor files, processes, directories, or devices on your system.'
+  
   source 'http://mmonit.com/monit/dist/monit-5.0.3.tar.gz' do
     post :install, 'mkdir /etc/monit'
     post :install, 'mkdir /etc/monit.d'
     post :install, 'mkdir /var/lib/monit'
   end
-  transfer "#{File.dirname(__FILE__)}/../templates/monit_init", "/etc/init.d/monit"
-  put "/etc/init.d/monit", open("#{File.dirname(__FILE__)}/../templates/monit_init").read, :mode => 0755
   
   requires :essentials, :monit_dependencies
   
-  config do 
-    post :install, 'mkdir /etc/monit'
-    post :install, 'mkdir /etc/monit.d'
-    post :install, 'mkdir /var/lib/monit'
-    put "/etc/init.d/monit", open("#{File.dirname(__FILE__)}/../templates/monit_init").read, :mode => 0755
-  end
-  
-  verify do
-    has_executable "monit"
-  end
+  #verify do
+  #  has_executable "monit"
+  #end
 end
 
+package :monit_init_d do
+  description "Monit init.d script."
+  requires :monit
+  
+  transfer "#{File.dirname(__FILE__)}/../templates/monit_init", "/etc/init.d/monit" do
+    post :install, "chmod +c /etc/init.d/monit"
+  end
+  
+  #verify do 
+  #  has_file "/etc/init.d/monit"
+  #end
+  
+end
+  
 package :monit_dependencies do
   description "Dependencies to build monit from source"
   apt 'flex byacc'
