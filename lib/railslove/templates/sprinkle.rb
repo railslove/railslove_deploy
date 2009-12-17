d2536db3b2c5c73c8b4694da12e1feb3669bbd05
Michael Bumann <michael@derbumi.com>
2009-12-17 13:14:16 +0100

    add some information about the installed packacges to the server policy

--- lib/railslove/templates/sprinkle.rb
+++ lib/railslove/templates/sprinkle.rb
@@ -2,24 +2,25 @@
 require "railslove/sprinkle"
 
 policy :passenger_stack, :roles => :app do
-  requires :webserver
-  requires :database
+  requires :webserver         # Apache
+  requires :database          # MySQL, SQLite
   
   # memcached
-  requires :memcached_daemon
-  requires :libmemcached
-  requires :memcached_conf
+  requires :memcached_daemon  # Memcached
+  requires :libmemcached      # libmemcached
+  requires :memcached_conf    # memcached-user, init.d config 
   
-  requires :scm
-  requires :ruby
-  requires :searchengine
-  requires :appserver
-  requires :rails_user
-  requires :database_driver
-  requires :usefull_gems
-  requires :image_magick
-  requires :ferm
-  requires :monit
+  requires :scm               # Git, SVN
+  requires :ruby              # Ruby Enterprise
+  requires :searchengine      # Sphinx
+  requires :appserver         # passenger
+  requires :rails_user        # special rails user with sudo rights
+  requires :database_driver   # Ruby database driver
+  requires :usefull_gems      # usefull, frequently needed gems
+  requires :image_magick      # image magick
+  requires :ferm              # ferm iptable configuration tool
+  requires :ferm_conf         # configure ferm (allow http, https and ssh)
+  requires :monit             # monit - system process monitoring
 end
 
 deployment do
