c7688fb76f26dc1f793581a5226a52a344b19d09
Michael Bumann <michael@derbumi.com>
2010-01-08 01:19:27 +0100

    make astrails-safe backup script configurable by setting a backup_script variable. (but still asume script is in config/server/

--- lib/railslove/recipes/backup.rb
+++ lib/railslove/recipes/backup.rb
@@ -2,7 +2,7 @@ namespace :backup do
   
   desc "Create a new backup using the astrails-safe config script from config/server/backup.rb"
   task :run do 
-    sudo "cd #{current_path}; /usr/local/ruby-enterprise/bin/astrails-safe -v #{current_path}/config/server/backup.rb"
+    sudo "cd #{current_path}; /usr/local/ruby-enterprise/bin/astrails-safe -v #{current_path}/config/server/#{fetch(:backup_script, "backup.rb")}"
   end
   
 end
