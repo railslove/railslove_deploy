704fc1515ea74072815da51669311f0082dd58ee
Michael Bumann <michael@derbumi.com>
2010-01-08 01:18:18 +0100

    don't require twitter credentials to be stored in the deploy script to run the twitter hook

--- lib/railslove/recipes/hooks.rb
+++ lib/railslove/recipes/hooks.rb
@@ -10,21 +10,22 @@ namespace :hooks do
       url = URI.parse("http://twitter.com/statuses/update.json")
       
       status = Capistrano::CLI.ui.ask("Update Twitter status to: (leave blank to skip)")
+      user =  fetch(:twitter_user, nil).nil? ? Capistrano::CLI.ui.ask("Twitter username: ") : fetch(:twitter_user, nil)
+      passwd = fetch(:twitter_password, nil).nil? ? Capistrano::CLI.password_prompt("Twitter password for @#{twitter_user}: ") : fetch(:twitter_password, nil)
       unless status.empty?
       
         http = Net::HTTP.new(url.host, 80)
         http.open_timeout = 20
       
-        headers = {'Authorization' => 'Basic ' + ["#{twitter_user}:#{twitter_password}"].pack('m').delete("\r\n")}
+        headers = {'Authorization' => 'Basic ' + ["#{user}:#{passwd}"].pack('m').delete("\r\n")}
       
         body = "status=#{CGI.escape(status)}"
         begin
           http.send(:post, url.path, body, headers) 
         rescue Timeout::Error => e
           puts "Timeout after 20s: Seems like Twitter is down."
-          puts "Use \"cap twitter:update\" to update Twitter status later w/o deploying"
+          puts "Use \"cap hooks:twitter:update\" to update Twitter status later w/o deploying"
         end
-        # if an exception is thrown, let them see it, they're a developer (most likely)
       end
     end
   end
