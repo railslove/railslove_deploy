61384143b7475b27aa2bef05f05e460deb96af71
Michael Bumann <michael@railslove.com>
2010-02-28 16:50:05 +0100

    update useful gems

--- lib/railslove/sprinkle/usefull_gems.rb
+++ lib/railslove/sprinkle/usefull_gems.rb
@@ -1,5 +1,5 @@
 package :usefull_gems do
-  USEFUL_GEMS ="aws-s3 astrails-safe bcrypt-ruby chronic daemons dm-core dm-more erubis eventmachine haml hpricot json json_pure memcache-client money mime-types net-sftp net-ssh newrelic_rpm oauth ParseTree rails RedCloth ruby-openid ruby2ruby RubyInline sinatra starling thor thoughtbot-factory_girl will_paginate wvanbergen-request-log-analyzer xml-simple xmpp4r xmpp4r-simple"
+  USEFUL_GEMS ="authlogic aws-s3 astrails-safe bcrypt-ruby bundler chronic couchrest couch_potato daemons devise dm-core dm-more erubis eventmachine find_by_param haml hpricot json json_pure memcache-client money mime-types net-sftp net-ssh newrelic_rpm oauth paperclip ParseTree rack rails RedCloth ruby-openid ruby2ruby RubyInline sinatra starling thor thoughtbot-factory_girl will_paginate wvanbergen-request-log-analyzer xml-simple xmpp4r xmpp4r-simple"
   gem USEFUL_GEMS
   
   verify do
