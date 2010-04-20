89e812d0afe05be1a354a5c6c12c788670975519
Michael Bumann <michael@railslove.com>
2010-04-20 20:28:56 +0200

    update useful_gems policy. only really basic gems should be installed by default

--- lib/railslove/sprinkle/usefull_gems.rb
+++ lib/railslove/sprinkle/usefull_gems.rb
@@ -1,5 +1,5 @@
 package :usefull_gems do
-  USEFUL_GEMS ="authlogic aws-s3 astrails-safe bcrypt-ruby bundler chronic couchrest couch_potato daemons devise dm-core dm-more erubis eventmachine find_by_param haml hpricot json json_pure memcache-client money mime-types net-sftp net-ssh newrelic_rpm oauth paperclip ParseTree rack rails RedCloth ruby-openid ruby2ruby RubyInline sinatra starling thor thoughtbot-factory_girl will_paginate wvanbergen-request-log-analyzer xml-simple xmpp4r xmpp4r-simple"
+  USEFUL_GEMS ="astrails-safe bcrypt-ruby builder bundler chronic daemons erubis eventmachine haml i18n json json_pure mail memcache-client mime-types net-sftp net-ssh ParseTree rack rack-test rack-mount rails ruby2ruby RubyInline text-format thor tzinfo wvanbergen-request-log-analyzer"
   gem USEFUL_GEMS
   
   verify do
