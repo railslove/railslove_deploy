e4ba5f940e82e96a701b1b79ce39d06375e7bd0d
Michael Bumann <michael@railslove.com>
2010-04-29 12:04:14 +0200

    update bundler recipe to install and symlink gems to shared dir. thanks @reddavis for this code

--- lib/railslove/recipes/bundler.rb
+++ lib/railslove/recipes/bundler.rb
@@ -1,12 +1,16 @@
-# from http://pivotallabs.com/users/chad/blog/articles/1208-automating-bundler-in-your-deploy
 namespace :deploy do
-  desc "installs Bundler if it is not already installed"
-  task :install_bundler, :roles => :app do
-    sudo "sh -c 'if [ -z `which bundle` ]; then echo Installing Bundler; sudo gem install bundler; fi'"
-  end
-
   desc "run 'bundle install' to install Bundler's packaged gems for the current deploy"
   task :bundle_install, :roles => :app do
-    run "cd #{release_path} && bundle install --relock"
+    run <<-CMD
+      mkdir -p #{shared_path}/.bundle &&
+      mkdir -p #{shared_path}/vendor/bundler_gems
+    CMD
+
+    run <<-CMD
+      ln -s #{shared_path}/.bundle #{latest_release}/.bundle &&
+      ln -s #{shared_path}/vendor/bundler_gems #{latest_release}/vendor/bundler_gems
+    CMD
+
+    run "cd #{latest_release} && /usr/local/ruby-enterprise/bin/bundle check || /usr/local/ruby-enterprise/bin/bundle install vendor/bundler_gems --disable-shared-gems --without development test"
   end
 end
