77352376d152096e6cbc2fe40720a1cd71b4597f
Mike Poltyn <mike@poltyn.com>
2011-05-13 20:55:56 +0200

    Motivation rejected

--- lib/railslove/sprinkle/passenger.rb
+++ lib/railslove/sprinkle/passenger.rb
@@ -1,10 +1,10 @@
-package :passenger_gem do
+package :passenger_gem do 
   description 'Phusion Passenger (aka mod_rails)'
-  version '2.2.15'
+  version '2.2.11'
   gem 'passenger'
   requires :ruby_enterprise
-
-  verify do
+  
+  verify do 
     has_gem 'passenger'
   end
 end
@@ -13,45 +13,45 @@ package :passenger_apache, :provides => :appserver do
   description "Installs and configures Passenger for use with apache webserver"
   # require the apache passenger module load/conf files before installation because of a sprinkle limitation.
   requires :apache, :passenger_gem, :passenger_module_load, :passenger_module_conf
-
-  passenger_version = '2.2.15'
-
-  noop do
+  
+  passenger_version = '2.2.11'
+  
+  noop do 
     pre :install, 'passenger-install-apache2-module --auto'
     post :install, "sudo a2enmod passenger"
     post :install, 'echo "NameVirtualHost *:80" | sudo tee -a /etc/apache2/httpd.conf'
     # Restart apache to enable changes
     post :install, '/etc/init.d/apache2 restart'
   end
-
+  
   verify do
     has_file "/usr/local/ruby-enterprise/lib/ruby/gems/1.8/gems/passenger-#{passenger_version}/ext/apache2/mod_passenger.so"
   end
 end
 
 package :passenger_module_load do
-
+  
   description "Uploads the passenger module load configuration. -sprinkle currently does not allow multiple installers per package, that's why the configuration files have it's own packages"
   requires :apache, :passenger_gem
-
+  
   #TODO: :render => true seems not to work? hello sprinkle?!
-  passenger_version = '2.2.15'
+  passenger_version = '2.2.11'
   transfer "#{File.dirname(__FILE__)}/../templates/passenger.load", "/etc/apache2/mods-available/passenger.load", :render => true
-
+  
   verify do
     has_file '/etc/apache2/mods-available/passenger.load'
   end
 end
 
 package :passenger_module_conf do
-
+  
   description "Uploads the passenger module conf configuration. -sprinkle currently does not allow multiple installers per package, that's why the configuration files have it's own packages"
   requires :apache, :passenger_gem
-
-  #TODO: :render => true seems not to work? hello sprinkle?!
-  passenger_version = '2.2.15'
+  
+  #TODO: :render => true seems not to work? hello sprinkle?!  
+  passenger_version = '2.2.11'
   transfer "#{File.dirname(__FILE__)}/../templates/passenger.conf", "/etc/apache2/mods-available/passenger.conf", :render => true
-
+  
   verify do
     has_file '/etc/apache2/mods-available/passenger.conf'
   end
@@ -59,10 +59,10 @@ end
 
 package :passenger_nginx, :provides => :appserver  do
   description "Installs and configures Passenger for use with the awesome nginx webserver"
-
+  
   requires :passenger_gem
-
-  noop do
+  
+  noop do 
     pre :install, "passenger-install-nginx-module --auto --auto-download"
   end
 end
